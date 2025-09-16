9e6a5ef8b886fe04c78a3539e7ed443c81f7b065
Michael Achumba <michaelachumba@gmail.com>
2025-09-16 21:42:04 +0100

    Adds JS script with addTask function

---
+++ script.js
@@ -0,0 +1,33 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const addButton = document.getElementById('add-task-btn'); // Selects Add Task Button
+    const taskInput = document.getElementById('task-input'); // Selects task input field
+    const taskList = document.getElementById('task-list'); // Selects ul for tasks
+
+    // Add Task Function
+    function addTask() {
+        const taskText = taskInput.value.trim();
+        // Confirm if input is empty
+        if (taskText === "") {
+            alert('Enter a task!');
+        }
+
+        if (taskText !== "") {
+            const li = document.createElement('li'); // creates list item
+            li.textContent = taskText; // sets text content
+
+            // Create remove button and give it a class
+            const removeButton = document.createElement('button');
+            removeButton.textContent = 'Remove';
+            removeButton.className = 'remove-btn';
+            // Listen for click to remove task when clicked
+            removeButton.addEventListener('click', function () {
+                li.remove();
+            });
+            // Append remove button to li element
+            li.appendChild(removeButton);
+            taskList.appendChild(li);
+            taskInput.value = '';
+        }
+        
+    }
+});
